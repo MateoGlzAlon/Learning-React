6e0af7392000b3477f7313a06c1ff81fd9537eec
MateoGonzalezz <91258099+MateoGonzalezz@users.noreply.github.com>
2024-08-26 20:48:14 +0200

    Contents from 00:00 to 34:12

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Learning-React
+
+Repository created while watching the video **[React Full Course for Free ⚛️ (2024)](https://www.youtube.com/watch?v=CgkZ7MvWUAA)** from **[Bro Code](https://www.youtube.com/@BroCodez)**.
---
+++ card-components/src/Card.jsx
@@ -0,0 +1,14 @@
+
+
+function Card() {
+
+    return (
+        <div className="card">
+            <img className="card-image" src="https://via.placeholder.com/150" alt="profile picture"></img>
+            <h2 className="card-title">Card</h2>
+            <p className="card-text">I am a Computer Science student</p>
+        </div>
+    )
+}
+
+export default Card
