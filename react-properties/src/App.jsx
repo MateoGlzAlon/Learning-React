4a1dee7b66bea4b4d9111295cf4a070f56afcc32
MateoGonzalezz <mateo.gonzalez.alonso@gmail.com>
2024-08-26 22:25:45 +0200

    Contents from 34:12 to 44:59

---
+++ react-properties/src/App.jsx
@@ -0,0 +1,12 @@
+import Student from './Student'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Student name="Mateo" age={21} isStudent={true} />
+    </>
+  )
+}
+
+export default App
